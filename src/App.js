7fb9f0d4f3d33297cc1a7e51bb4b193261d53ed3
KhaNam <khanamdev@gmail.con>
2021-01-27 14:19:28 +0700

    ud

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomeScreen from "./screens/HomeScreen";
 import Provider from "./contexts/Provider";
-import AppContext from "./contexts/Context";
+
 
 function App() {
   return (
-      <HomeScreen/>
+      <Provider>
+        <HomeScreen/>
+      </Provider>
   );
 }
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import FormTodoComponent from "../components/FormTodoComponent";
 import ListTaskComponent from "../components/ListTaskComponent";
-import Provider from "../contexts/Provider";
 
 const HomeScreen = () => {
 
     return (
-        <main id="main-container">
-            <section id="wrap-todo">
+            <main id="main-container">
+                <section id="wrap-todo">
 
-                <div className="container">
-                    <h1>My Todos</h1>
-                    <Provider>
-                        <FormTodoComponent />
-                        <ListTaskComponent />
-                    </Provider>
-                </div>
-            </section>
-        </main>
+                    <div className="container">
+                        <h1>My Todos</h1>
+                        <FormTodoComponent/>
+                        <ListTaskComponent/>
+                    </div>
+                </section>
+            </main>
+
     )
 }
 
